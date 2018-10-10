7b430786df5354703bc28d3acc097693af440493
Lucas Bares <lucasbaresm@gmail.com>
2018-10-10 18:14:55 -0300

    Initial Commit

---
+++ ConsoleApp1/Tests/FacebookOpen.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using NUnit.Framework;
+using ConsoleApp1.Utils;
+
+namespace ConsoleApp1
+{
+    class FacebookOpen : BaseComponents
+    {
+        [Test]
+        public void OpenFacebook()
+        {
+            Utils.BaseComponents.Equal
+            driver.Url = "https://www.google.com";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Utils/BaseComponents.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace ConsoleApp1.Utils
+{
+    class BaseComponents
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Inicializate()
+        {
+            driver = new ChromeDriver(@"C:\Users\lucas.mazzini\source\repos\ConsoleApp1\ConsoleApp1");
+        }
+
+        [TearDown]
+        public void Exit()
+        {
+            driver.Quit();
+        }
+    }
+}
