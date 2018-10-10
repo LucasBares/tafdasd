e365d017d10782ae3b78a62435b53de8e51662a3
Lucas Barés <lucasbaresm@gmail.com>
2018-10-10 20:52:15 -0300

    Added Facebook search

--- ConsoleApp1/Tests/FacebookOpen.cs
+++ ConsoleApp1/Tests/FacebookOpen.cs
@@ -1,17 +1,45 @@
 ﻿using OpenQA.Selenium;
 using NUnit.Framework;
 using ConsoleApp1.Utils;
+using System;
 
 namespace ConsoleApp1
 {
     class FacebookOpen : BaseComponents
     {
-        public BaseComponents salir = new BaseComponents();
+        public BaseComponents utils = new BaseComponents();
+
         [Test]
         public void OpenFacebook()
         {
             driver.Url = "https://www.google.com";
-            salir.Exit();
+
+            driver.FindElement(By.Name("q")).SendKeys("Facebook");
+            driver.FindElement(By.XPath("//*[@id='tsf']/div[2]/div[3]/center/input[1]")).Click();
+
+            string Title = driver.Title;
+
+            if(Title == "Facebook - Buscar con Google")
+            {
+                var FUrl = driver.FindElement(By.XPath("//*[@id='rso']/div/div/div/div/div/div/a/div/cite")).Text;
+                driver.Url = FUrl;
+                if(Title == "Facebook - Inicia sesión o regístrate")
+                {
+                    Console.WriteLine("The test was successfull");
+                }
+                else
+                {
+                    Console.WriteLine("The test failed");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Page not found");
+            }
+            
+
+
+            utils.Exit();
         }
 
     }
